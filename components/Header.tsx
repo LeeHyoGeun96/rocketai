e013296f6b8a9857f47df68fc09aa6ff0ec569ca
hyogeun <stonepagoda8@gmail.com>
2025-05-24 00:48:20 +0900

    feat: header 항목 추가
    - layout 위치 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Header() {
-  return <div className="w-screen">Header</div>;
+  const pathname = usePathname();
+
+  const activeLinkStyle = "underline underline-offset-4 decoration-sky-500";
+
+  return (
+    <div className="w-screen">
+      <ul className="flex mx-auto w-full max-w-screen-lg justify-end gap-4  items-center">
+        <li>
+          <Link
+            href="/"
+            className={`text-2xl hover:text-sky-600 transition-colors ${
+              pathname === "/" ? activeLinkStyle : ""
+            }`}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/custom"
+            className={`text-2xl hover:text-sky-600 transition-colors ${
+              pathname === "/custom" ? activeLinkStyle : ""
+            }`}
+          >
+            Custom
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-import Layout from "@/components/Layout";
-
 export default function Home() {
   return (
-    <Layout>
-      <main className="flex flex-col max-w-md w-full h-full bg-red-500 mx-auto"></main>
-    </Layout>
+    <main className="flex flex-col max-w-md w-full h-full bg-red-500 mx-auto"></main>
   );
 }
